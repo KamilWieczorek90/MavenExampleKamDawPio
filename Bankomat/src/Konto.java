d25e4e36e471c9f3109afc647f1d5ff19f42aed9
kamil <kamilwieczorek90@gmail.com>
2019-05-18 22:17:24 +0200

    test

---
+++ Bankomat/src/Konto.java
@@ -0,0 +1,28 @@
+//public class Konto {
+//
+//
+//    private double stan = 0;
+//
+//    public double wplac(double dodaj){
+//        stan+= dodaj;
+//        return stan;
+//    }
+//    public double wyplac(double odejmij){
+//        stan-= odejmij;
+//        return stan;
+//    }
+//    private void wyswietlStan(){
+//        System.out.print("Stan Twojego konta wynoi: "+ stan);
+//    }
+//    public void przelej(double przelew, Konto k){
+//        k.setStan(k.stan+przelew);
+//    }
+//
+//    public double getStan() {
+//        return stan;
+//    }
+//
+//    public void setStan(double stan) {
+//        this.stan = stan;
+//    }
+//}
